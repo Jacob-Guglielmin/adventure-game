a6c3c7b81035e697bdbfe74ea57f137cf3d7fad9
Jacob-Guglielmin <jacobguglielmin@gmail.com>
2020-10-22 08:59:35 -0600

    Fixed a bug where rooms could be missing

--- js/mapGen.js
+++ js/mapGen.js
@@ -27,7 +27,12 @@ variance = 0.2,
 
 roomSize = ((mapWidth + mapHeight) / 2) / 7,
 
-branches = 4;
+branches = 4,
+
+decorationRatios = {
+    water: 0.2,
+    grass: 0.3,
+};
 
 //Seeded random function, many thanks to David Bau (http://davidbau.com/archives/2010/01/30/random_seeds_coded_hints_and_quintillions.html)
 !function(f,a,c){var s,l=256,p="random",d=c.pow(l,6),g=c.pow(2,52),y=2*g,h=l-1;function n(n,t,r){function e(){for(var n=u.g(6),t=d,r=0;n<g;)n=(n+r)*l,t*=l,r=u.g(1);for(;y<=n;)n/=2,t/=2,r>>>=1;return(n+r)/t}var o=[],i=j(function n(t,r){var e,o=[],i=typeof t;if(r&&"object"==i)for(e in t)try{o.push(n(t[e],r-1))}catch(n){}return o.length?o:"string"==i?t:t+"\0"}((t=1==t?{entropy:!0}:t||{}).entropy?[n,S(a)]:null==n?function(){try{var n;return s&&(n=s.randomBytes)?n=n(l):(n=new Uint8Array(l),(f.crypto||f.msCrypto).getRandomValues(n)),S(n)}catch(n){var t=f.navigator,r=t&&t.plugins;return[+new Date,f,r,f.screen,S(a)]}}():n,3),o),u=new m(o);return e.int32=function(){return 0|u.g(4)},e.quick=function(){return u.g(4)/4294967296},e.double=e,j(S(u.S),a),(t.pass||r||function(n,t,r,e){return e&&(e.S&&v(e,u),n.state=function(){return v(u,{})}),r?(c[p]=n,t):n})(e,i,"global"in t?t.global:this==c,t.state)}function m(n){var t,r=n.length,u=this,e=0,o=u.i=u.j=0,i=u.S=[];for(r||(n=[r++]);e<l;)i[e]=e++;for(e=0;e<l;e++)i[e]=i[o=h&o+n[e%r]+(t=i[e])],i[o]=t;(u.g=function(n){for(var t,r=0,e=u.i,o=u.j,i=u.S;n--;)t=i[e=h&e+1],r=r*l+i[h&(i[e]=i[o=h&o+t])+(i[o]=t)];return u.i=e,u.j=o,r})(l)}function v(n,t){return t.i=n.i,t.j=n.j,t.S=n.S.slice(),t}function j(n,t){for(var r,e=n+"",o=0;o<e.length;)t[h&o]=h&(r^=19*t[h&o])+e.charCodeAt(o++);return S(t)}function S(n){return String.fromCharCode.apply(0,n)}if(j(c.random(),a),"object"==typeof module&&module.exports){module.exports=n;try{s=require("crypto")}catch(n){}}else"function"==typeof define&&define.amd?define(function(){return n}):c["seed"+p]=n}("undefined"!=typeof self?self:this,[],Math);
@@ -78,21 +83,36 @@ function fillMap() {
         rooms[0].availableDoors.shift();
     }
 
-    //Generate one more room outside of two rooms
-    if(addDoor(rooms[rooms.length - 2])) {
-        newRoom(getOutside(rooms[rooms.length - 2].availableDoors[0]), rooms[rooms.length - 2].availableDoors[0]);
-        rooms[rooms.length - 1].availableDoors.shift();
-    }
+    if (rooms.length == 5) {    
+        //Generate one more room outside of two rooms
+        if(addDoor(rooms[3])) {
+            newRoom(getOutside(rooms[3].availableDoors[0]), rooms[3].availableDoors[0]);
+            rooms[3].availableDoors.shift();
+        }
 
-    if(addDoor(rooms[rooms.length - 2])) {
-        newRoom(getOutside(rooms[rooms.length - 2].availableDoors[0]), rooms[rooms.length - 2].availableDoors[0]);
-        rooms[rooms.length - 1].availableDoors.shift();
-    }
+        if(addDoor(rooms[4])) {
+            newRoom(getOutside(rooms[4].availableDoors[0]), rooms[4].availableDoors[0]);
+            rooms[4].availableDoors.shift();
+        }
 
-    //Generate one more room outside of the most recent room
-    if(addDoor(rooms[rooms.length - 1])) {
-        newRoom(getOutside(rooms[rooms.length - 1].availableDoors[0]), rooms[rooms.length - 1].availableDoors[0]);
-        rooms[rooms.length - 1].availableDoors.shift();
+        if (rooms.length == 7) {
+            //Generate one more room outside of the most recent room
+            if(addDoor(rooms[6])) {
+                newRoom(getOutside(rooms[6].availableDoors[0]), rooms[6].availableDoors[0]);
+                rooms[6].availableDoors.shift();
+            }
+
+            //Reset if rooms were missed
+            if (rooms.length != 8) {
+                resetMap();
+            } else {
+                //addDecoration();
+            }
+        } else {
+            resetMap();
+        }
+    } else {
+        resetMap();
     }
 }
 
@@ -309,21 +329,6 @@ function addDoor(room, retry = false) {
 }
 
 /**
- * Removes a door from the map
- */
-function removeDoor(coords) {
-    //Remove it from the room
-    var room = getTile(coords[0], coords[1]).room;
-    var doorTile = getRoomTile(rooms[room], coords[0], coords[1]);
-    rooms[room].tiles.splice(rooms[room].tiles.indexOf(doorTile), 1);
-    rooms[room].tiles.push({x: coords[0], y: coords[1], type: TILES.WALL});
-
-    //Remove it from the map
-    mapData[coords[1]][coords[0]].type = TILES.WALL;
-    mapCells[coords[1]][coords[0]].style.backgroundColor = TILES.WALL;
-}
-
-/**
  * Returns the location of an empty tile next to a location, if there are none, returns -1
  */
 function getOutside(location) {
@@ -461,8 +466,6 @@ function buildRoom(room) {
         if (buildAttempts <= 10) {
             buildAttempts++;
             newRoom([room.x, room.y], room.parentDoor);
-        } else {
-            removeDoor(room.parentDoor);
         }
     }
 }
